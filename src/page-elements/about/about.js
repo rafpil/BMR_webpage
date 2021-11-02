de6fd36a36eab1ad53bc300a2a52da991cecdf4e
Rafal <Rafal>
2021-11-02 21:24:12 +0100

    Added About section

---
+++ src/page-elements/about/about.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+
+
+import Section from "../../components/section/section";
+import Column from "../../components/column/column";
+
+import "./about.scss";
+
+const About = () => {
+  return (
+    <Section name="about">
+      <Column type="image" position="left">
+        <StaticImage className="img" src="./img/bg1.jpg" alt="Obraz" />
+      </Column>
+      <Column type="text" position="right">
+        <h2 className="section-header">O firmie</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit
+          quasi, totam nesciunt est iusto mollitia illum eveniet id similique
+          modi voluptas recusandae nostrum tenetur voluptatem officia at velit
+          voluptates hic? Dignissimos culpa aut quia omnis quasi dolore voluptas
+          magnam vero, temporibus suscipit! Atque iusto aepe adipisci,
+          laudantium illum animi quia  dolores dicta inventore praesentium vitae
+          fuga tempora, sequi quae suscipit. Alias, itaque tempore numquam
+          facilis veritatis ex quam laudantium aspernatur sed, et re doloribus
+          in voluptates fugiat cum ipsa quae accusamus? Corporis fugit non odio
+          quos in asperio res deserunt explicabo!
+        </p>
+      </Column>
+    </Section>
+  );
+};
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,16 @@
 import * as React from "react"
-import { Link } from "gatsby"
 
 import Seo from "../components/seo"
 import "../styles_global/styles.scss";
 
+// page elements
+import About from "../page-elements/about/about";
+
+
 const IndexPage = () => (
   <>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <button className="btn btn-danger">tset</button>
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+    <About />
   </>
 )
 
