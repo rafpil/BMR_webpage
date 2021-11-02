6e1ca0d3add85c6f30c5007f17ba975ee6a73e4d
Rafal <Rafal>
2021-11-02 23:09:48 +0100

    Added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+            "Work Sans\:300, 400, 500",
+          ],
+      },
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
