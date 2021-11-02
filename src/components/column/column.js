08b26fc5ad0304cdebf8094175010cf30dc22ec9
Rafal <Rafal>
2021-11-02 21:23:36 +0100

    Added column component

---
+++ src/components/column/column.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+
+import "./column.scss";
+
+const Column = (props) => {
+  return (
+    <Col
+      md={6}
+      className={`column-${props.type} column-${props.type}--${props.position} ${props.type === "image" & props.position === "left" ? 'order-1 order-md-0' : ''}`}
+    >
+      {props.children}
+    </Col>
+  );
+};
+
+export default Column;
